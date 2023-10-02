6c8c4e46661e9290d47b7d5bf448cabaebe54694
david.fonseca <fonsecasoundcruz@gmail.com>
2023-10-02 02:06:55 -0600

    create to do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from './shared.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'to-do-app';
+export class AppComponent implements OnInit {
+  //agregar servicio como proveedor
+  constructor(private service: SharedService) {}
+
+  ngOnInit(): void {
+    this.refreshNotes();
+  }
+
+  notes: any = [];
+
+  refreshNotes() {
+    this.service.getNotes().subscribe((res) => {
+      this.notes = res;
+    });
+  }
+
+  addNotes(newNote: string) {
+    this.service.addNote(newNote).then((res) => {
+      console.log(res);
+      this.refreshNotes();
+    });
+  }
+
+  deleteNotes(id: string) {
+    this.service.deleteNote(id).then((res) => {
+      console.log(res);
+      this.refreshNotes();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//Agregamos módulos para firebase
+import { provideFirebaseApp, getApp, initializeApp } from "@angular/fire/app";
+import {getFirestore, provideFirestore} from "@angular/fire/firestore";
+import { SharedService } from './shared.service';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAELnwXt1aRXMRkBpf4MzY2EwTSzs9rKU",
+  authDomain: "to-do-app-ad647.firebaseapp.com",
+  projectId: "to-do-app-ad647",
+  storageBucket: "to-do-app-ad647.appspot.com",
+  messagingSenderId: "328990237136",
+  appId: "1:328990237136:web:bdbd434bf8f089ef4b19d5",
+  measurementId: "G-WQ6S7J2DSK"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //importamos
+    provideFirebaseApp(()=> initializeApp (firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ],
-  providers: [],
+  providers: [
+    SharedService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
